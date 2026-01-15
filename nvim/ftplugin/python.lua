6ea665941d1cb0b74a5b96f69377fa0dd8b90bce
BrnPrs <parisbruno85@gmail.com>
2026-01-15 18:20:43 +0100

    pylsp plugins config

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,5 +1,6 @@
 -- Exit if the Python LSP server isn't available
 if vim.fn.executable('pylsp') ~= 1 then
+  vim.notify("pylsp not found!", vim.log.levels.WARN, { title = "Nvim-config" })
   return
 end
 
@@ -14,10 +15,44 @@ local root_files = {
 
 -- vim.lsp.set_log_level("debug")
 
+local venv_path = os.getenv("VIRTUAL_ENV")
+local py_path = nil
+if venv_path then
+  py_path = venv_path .. '/bin/python'
+else
+  py_path = vim.fn.exepath('python3') or vim.fn.exepath('python')
+end
+
 -- Start the Python LSP server
 vim.lsp.start {
   name = 'pylsp',
   cmd = { 'pylsp' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
+  settings = {
+    pylsp = {
+      plugins = {
+        -- formatter options
+        black = { enabled = true },
+        autopep8 = { enabled = false },
+        yapf = { enabled = false },
+        -- linter options
+        pylint = { enabled = true, executable = "pylint" },
+        ruff = { enabled = false },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        -- type checker
+        pylsp_mypy = {
+          enabled = true,
+          overrides = { "--python-executable", py_path, true },
+          report_progress = true,
+          live_mode = false
+        },
+        -- auto-completion options
+        jedi_completion = { fuzzy = true },
+        -- import sorting
+        isort = { enabled = true },
+      }
+    }
+  }
 }
